d22a59c344eb2636d7ee77c8f7ace94282ea2d4f
Dustan Kasten <dustan.kasten@gmail.com>
2016-10-28 15:38:46 -0400

    product finder

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -154,3 +154,85 @@ $('#myModal').on('shown.bs.modal', function () {
 $('#confirm').click(function(){
     $("#underage").fadeOut("slow");
 });
+
+(function () {
+    // https://gist.github.com/padolsey/6008842
+    var makeInterpolator = (function() {
+        var rc = {'\n': '\\n', '\"': '\\\"', '\u2028': '\\u2028', '\u2029': '\\u2029'};
+        return function makeInterpolator(str) {
+            return new Function(
+                'o',
+                'return "' + (
+                    str
+                    .replace(/["\n\r\u2028\u2029]/g, function($0) {
+                    return rc[$0];
+                    })
+                    .replace(/\{([\s\S]+?)\}/g, '" + o["$1"] + "')
+                ) + '";'
+                );
+        };
+    }());
+
+    function getQuery (postalCode) {
+        return [
+            'query {',
+                // customize the zip based on what the user is looking at
+                'locations(brand:ALL, zip:' + postalCode + ') {',
+                    // all the fields we care about for our UI
+                    'name street city state zip lat long distance',
+                '}',
+            '}',
+        ].join('');
+    }
+
+    function request(postalCode) {
+        if (finder.insertBefore) {
+            const text = document.createElement('div');
+            text.style = 'margin: 1em auto 3em;';
+            text.textContent = 'Searching in ' + postalCode + '...';
+            finder.insertBefore(text, finder.firstChild);
+        }
+
+        var protocol = location.protocol !== 'file:' ? location.protocol : 'http:';
+        fetch(protocol + '//stout-brand-finder.herokuapp.com/graphql?query=' + encodeURIComponent(getQuery(postalCode)))
+            .then(function(resp) { return resp.json(); })
+            .then(function (data) {
+                draw(data.data.locations);
+                return data;
+            })
+            .catch(function (err) {
+                console.log(err);
+                return err;
+            });
+    }
+
+    const finder = document.getElementById('finder-box');
+    const form = document.getElementById('search-form');
+    const input = document.getElementById('zip-field');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        request(input.value);
+    }, false);
+
+    const tmpl = [
+        '<div class="col-md-4">',
+            '<div class="dark-box box-hover">',
+                '<h2><i class="fa fa-map-marker"></i><span>{name}</span></h2>',
+                '<p>{street} {city} {state} {zip}</p>',
+                '<a href="http://maps.google.com/?q={name} {street} {city} {zip}"><p>link to map</p></a>',
+            '</div>',
+        '</div>'
+    ].join('');
+    const template = makeInterpolator(tmpl);
+
+    const draw = (locations) => {
+        if (locations.length === 0) {
+            finder.innerHTML = '<div>Unfortunately our products are not available in this location</div>';
+        } else {
+            finder.innerHTML = locations.reduce((html, location) => html + template(location), '');
+        }
+    };
+
+    request(input.value || '28086');
+})();
+
